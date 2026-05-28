d6da07b08d1ba5019946e69e2d49224e99409d28
marycamaraa <mary.g.camara@bbc.co.uk>
2026-05-28 22:30:54 +0100

    chore: add items to graphics navbar

--- src/components/GraphicsNavbar/index.jsx
+++ src/components/GraphicsNavbar/index.jsx
@@ -1,5 +1,21 @@
+import { Link } from 'react-router-dom'
+import './index.css'
+
 function GraphicsNavbar() {
-  return <></>
+  return (
+    <>
+      <nav className="graphics-navbar">
+        <ul className="graphics-nav-items">
+          <li>
+            <Link to="/">Cherry Pick</Link>
+          </li>
+          <li>
+            <Link to="/">Check Off</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
 }
 
 export default GraphicsNavbar
