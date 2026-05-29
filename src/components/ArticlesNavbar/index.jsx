55bcbf90bf177a5e3b9086dba691e96246d2d5bb
marycamaraa <mary.g.camara@bbc.co.uk>
2026-05-29 15:30:28 +0100

    chore: add profile icon

--- src/components/ArticlesNavbar/index.jsx
+++ src/components/ArticlesNavbar/index.jsx
@@ -1,21 +1,25 @@
 import { Link } from 'react-router-dom'
 import logo from '../../assets/logo.png'
 import notification from '../../assets/notification.png'
+import profileImg from '../../assets/profileImg.png'
+
 import './index.css'
 
 function ArticlesNavbar() {
   return (
     <nav className="articles-navbar-container">
-      <Link to="/">
-        <img className="logo" src={logo} />{' '}
-      </Link>
-      <Link to="/">
-        <img className="notification-icon" src={notification} />
-      </Link>
-      <ul className="nav-links">
+      <ul className="articles-nav-links">
+        <Link to="/">
+          <img className="articles-logo" src={logo} />{' '}
+        </Link>
+        <Link to="/profile">
+          <img className="profile-icon" src={profileImg} />
+        </Link>
+        <Link to="/">
+          <img className="notification-icon" src={notification} />
+        </Link>
         <li>
           <Link to="/home">Home</Link>
-          {''}
         </li>
         <li>
           <Link to="/news">News</Link>
