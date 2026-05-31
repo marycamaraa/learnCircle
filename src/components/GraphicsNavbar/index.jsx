b2a202eabeff84fc9b5694e708eb96c589a5d8e9
marycamaraa <mary.g.camara@bbc.co.uk>
2026-05-31 21:53:25 +0100

    chore: styling

--- src/components/GraphicsNavbar/index.jsx
+++ src/components/GraphicsNavbar/index.jsx
@@ -1,11 +1,18 @@
 import { Link } from 'react-router-dom'
+import graphicsIcon from '../../assets/graphicsIcon.png'
 import './index.css'
 
 function GraphicsNavbar() {
   return (
     <>
+      <div className="top-backgroud"> Cherry</div>
       <nav className="graphics-navbar">
         <ul className="graphics-nav-items">
+          <li>
+            <Link to="/">
+              <img className="graphics-logo" src={graphicsIcon} />
+            </Link>
+          </li>
           <li>
             <Link to="/">Cherry Pick</Link>
           </li>
