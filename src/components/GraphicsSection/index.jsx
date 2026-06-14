67a9980e1899779b7d1f543c1a1815335b557240
marycamaraa <mary.g.camara@bbc.co.uk>
2026-06-14 19:11:54 +0100

    feat: add delete button

--- src/components/GraphicsSection/index.jsx
+++ src/components/GraphicsSection/index.jsx
@@ -1,31 +1,48 @@
 import { useState } from 'react'
-
-import './index.css'
+import styles from './index.module.css'
 
 function GraphicsSections() {
   const [task, setTask] = useState('')
   const [tasks, setTasks] = useState([])
 
-  // helper function
+  // function hold the logic. funtion will run when a user clicks the add button
   const addTask = () => {
-    if (!task.trim()) return
+    if (!task.trim() === '') return // trim removes white space from beginning to end
 
-    setTasks([...tasks, task])
-    setTask('')
+    setTasks([...tasks, task]) // spread operator copies all existing items and add new task
+    setTask('') // clears the input field after adding task
   }
 
+  //delete function
+  const deleteTask = (indexToDelete) => {
+    setTasks(tasks.filter((_, index) => index !== indexToDelete))
+  }
   return (
-    <>
-      <h1>My to do list</h1>
-      <input value={task} onChange={(e) => setTask(e.target.value)} />
-      <button onClick={addTask}>Add</button>
+    <div className={styles.container}>
+      <h2 className={styles.heading}> To Do List</h2>
+      <div className={styles.inputContainer}>
+        <input type="text" value={task} onChange={(e) => setTask(e.target.value)} placeholder="enter a task" />
 
-      {/* <ul>
-        {task.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul> */}
-    </>
+        <button className={styles.button} onClick={addTask}>
+          Add
+        </button>
+        <button> Clear</button>
+      </div>
+
+      <div className="items-container">
+        <div />
+        <ul className={styles.toDoList}>
+          {tasks.map((item, index) => (
+            <li className={styles.toDoListItem} key={index}>
+              <span> {item} </span>
+              <button className={styles.deleteButton} onClick={() => deleteTask(index)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   )
 }
 
